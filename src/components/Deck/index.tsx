fd283b4e68e37ade1c7a9eb1c087ac4f509cafe5
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2023-09-20 09:30:19 -0300

    Criada a listagem de decks.

--- src/components/Deck/index.tsx
+++ src/components/Deck/index.tsx
@@ -1,46 +1,105 @@
 import deckService from "../../services/DeckService";
-import { Card } from "antd";
+import { Button, Card, FloatButton, Tooltip, notification, Popconfirm } from "antd";
 import React, { useEffect, useState } from "react";
 import { CardPage } from "../../dtos/CardPage";
 import { v4 as uuidv4 } from 'uuid';
 import "./deck.css";
 import { useRouter } from "next/router";
+import { CustomerServiceOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
+import AddCard from "../AddCard";
+import cardService from "@/services/CardService";
+import type { NotificationPlacement } from 'antd/es/notification/interface';
 
 const { Meta } = Card;
 function Deck() {
   const [cards, setCards] = useState<CardPage>();
   const router = useRouter();
+  const [addCardVisible, setAddCardVisible] = useState(false);
+  const [api, contextHolder] = notification.useNotification();
+  const Context = React.createContext({ name: 'Default' });
 
-  
-  useEffect(() => {
-    async function loadData() {
-      if (router.query.id !== undefined) {
-        const response = await deckService.findCards(Number(router.query.id), 10, 0);
-        setCards(response.data);
-      }
-      
+  async function loadData() {
+    if (router.query.id !== undefined) {
+      const response = await deckService.findCards(Number(router.query.id), 30, 0);
+      setCards(response.data);
     }
+    
+  }
+
+  function openNotificationError (message: string) {
+    const placement: NotificationPlacement = 'topRight';
+    api.error({
+      message: `Notification`,
+      description:  message,
+      placement,
+    });
+  };
+
+  function openNotificationSuccess (message: string) {
+    const placement: NotificationPlacement = 'topRight';
+    api.success({
+      message: `Notification`,
+      description:  message,
+      placement,
+    });
+  };
+  useEffect(() => {  
     loadData();
   }, [router.query.id]);
 
+  async function deleteCard(cardId: Number) {
+    try {
+      const response = await cardService.delete(Number(router.query.id), cardId);
+      if (response.status === 204) {
+        openNotificationSuccess("Operação realizada com sucess.");
+        loadData();
+      }
+    } catch (error: any) {
+      openNotificationError(error.message)
+    }
+  }
 
   return (
     <main>
       <h1 className='title'>{'Deck de ' + router.query.description}</h1>
-     
+      {contextHolder}
       <div className='card-container'>
       {cards && cards.content.map((card)=> {
           return (
-            <Card key={uuidv4()}
-              hoverable
-              style={{ width: 240 }}
-              cover={<img alt="example" src={card.image} />}
-            >
-              <Meta title={card.name} description={card.description} />
-            </Card>
+            <Tooltip  key={uuidv4()} placement="bottom" title={card.description}>
+              <Card key={uuidv4()}
+                hoverable
+                style={{ width: 250 }}
+                cover={<img alt="example" src={card.image} />}
+               
+              >
+                <Meta style={{ width: 250 }}/>
+
+
+                <Popconfirm
+                  title="Remover carta"
+                  description="Você tem certeza que deseja remover essa carta do deck?"
+                  onConfirm={()=> deleteCard(card.cardApiId)}
+                  onCancel={()=> console.log("sd")}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Button className={"btn-delete"} type="primary" danger icon={<DeleteOutlined />} 
+                  />
+                </Popconfirm>
+              </Card>
+            </Tooltip>
           )
         })} 
       </div>
+      <FloatButton
+        shape="square"
+        type="primary"
+        style={{ right: 24}}
+        icon={ <PlusOutlined />}
+        onClick={()=> setAddCardVisible(true)}
+      />
+      <AddCard open={addCardVisible} setOpen={setAddCardVisible} loadData={loadData}  />
     </main>
   )
 }
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import auth from '../../services/Auth';
-import { Card } from 'antd';
+import { Button, Card, Popconfirm, notification } from 'antd';
 import "./home.css";
 import { DeckDTO } from '../../dtos/deckDTO';
 import deckService from '../../services/DeckService';
@@ -8,6 +8,8 @@ import { useRouter } from 'next/router';
 import { v4 as uuidv4 } from 'uuid';
 import Banner from '../Banner';
 import RegisterDeck from '../RegisterDeck';
+import { DeleteOutlined } from '@ant-design/icons';
+import type { NotificationPlacement } from 'antd/es/notification/interface';
 
 const { Meta } = Card;
 function Home() {
@@ -15,9 +17,24 @@ function Home() {
   const router = useRouter();
   const [openCreateDeck, setOpenCreateDeck] = useState<boolean>(false);
 
+  const [api, contextHolder] = notification.useNotification();
+  const Context = React.createContext({ name: 'Default' });
+
   function logout(e:React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     e.preventDefault();
     auth.logout();
+  }
+
+  async function deleteDeck(deckId: Number){
+    try {
+      const response = await deckService.remove(deckId);
+      if (response.status === 204) {
+        openNotificationSuccess("Operação realizada com sucesso.");
+        loadData();
+      }
+    } catch (error: any) {
+      openNotificationError(error.message);
+    }
   }
 
   async function loadData() {
@@ -28,20 +45,40 @@ function Home() {
     loadData();
   }, []);
 
+
+  function openNotificationError (message: string) {
+    const placement: NotificationPlacement = 'topRight';
+    api.error({
+      message: `Notification`,
+      description:  message,
+      placement,
+    });
+  };
+
+  function openNotificationSuccess (message: string) {
+    const placement: NotificationPlacement = 'topRight';
+    api.success({
+      message: `Notification`,
+      description:  message,
+      placement,
+    });
+  };
+
   return (
     <main className="">
-
+      {contextHolder}
       <Banner />
       <h1 className='title'>Sua Lista de Decks</h1>
-      <div className='card-container'>
+      <div className='deck-container'>
         <RegisterDeck open={openCreateDeck} setOpen={setOpenCreateDeck} loadDecks={loadData} />
+        
         <Card className='card-new-deck' key={`card-deck-${uuidv4()}`}
               hoverable
-              style={{ width: 240, height:240 }}
+              style={{ width: 240}}
               cover={<img alt="deck" src="card.png" />}
               onClick={()=> setOpenCreateDeck(true)}
             >
-              <Meta title={"Criar Deck"} description='' />
+              <Meta title={"Criar Deck"} description='' style={{ width: 240 }} />
         </Card>
 
         {decks && decks.map((deck)=> {
@@ -52,13 +89,24 @@ function Home() {
             <Card key={`card-deck-${deck.id}`}
               hoverable
               style={{ width: 240 }}
-              cover={<img alt="example" src="deck.png" />}
-              onClick={navigateDeckPage}
+              cover={<img alt="example" src="deck.png" onClick={navigateDeckPage} />}   
             >
-              <Meta title={deck.description} description='' />
+              <Meta title={deck.description} description='' style={{ width: 240 }}/>
+
+              <Popconfirm
+                  title="Remover Deck"
+                  description="Você tem certeza que deseja remover esse deck?"
+                  onConfirm={()=> deleteDeck(deck.id)}
+                  onCancel={()=> console.log("sd")}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Button className={"btn-delete"} type="primary" danger icon={<DeleteOutlined />} 
+                  />
+                </Popconfirm>
             </Card>
           )
-        })} 
+        })}
       </div>
     </main>
   );
